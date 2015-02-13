833fc6d41327d03f25274b73e53e530f7d614709
Evan Culler <evmicu@gmail.com>
2015-02-13 15:57:57 -0500

    Added Ajax to populate option menus
    Dropdown now populate from Riot’s static data api

--- Javascript/ajaxForms.js
+++ Javascript/ajaxForms.js
@@ -16,15 +16,41 @@ function populateSelect(){
   var champSelect = $('#champSelect').val();
   $('#items').html(champSelect);
   $('#item').html('');
-
+  //List all Champions (Why the hell would the name Wukong MonkeyKing)
   if(champSelect == 'Champs'){
-    champs.forEach(function(t){
-      $('#item').append('<option>'+t+'</option>');
+    $.ajax({
+        url: 'http://ddragon.leagueoflegends.com/cdn/4.20.1/data/en_US/champion.json',
+        type: 'GET',
+        dataType: 'json',
+        data: {
+
+        },
+        success: function (json) {
+            for (var name in json.data) {
+              $('#item').append('<option>'+ json.data[name].name +'</option>');
+            }
+        },
+        error: function () {
+            alert('that shit cray');
+        }
     });
   }
   if(champSelect == 'Items'){
-    champs2.forEach(function(t){
-      $('#item').append('<option>'+t+'</option>')
+    $.ajax({
+        url: 'http://ddragon.leagueoflegends.com/cdn/4.20.1/data/en_US/item.json',
+        type: 'GET',
+        dataType: 'json',
+        data: {
+
+        },
+        success: function (json) {
+            for (var name in json.data) {
+              $('#item').append('<option value=id' + name + '>' + json.data[name].name +'</option>');
+            }
+        },
+        error: function () {
+            alert('that shit cray');
+        }
     });
   }
 
